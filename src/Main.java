1fca4be1b527bfea0a78e8e69984e718c99467c0
Igor <99yearago99@gmail.com>
2022-09-19 14:33:25 +0300

    First task

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,14 @@ public class Main {
         System.out.println("Общий вес боксеров " + generalBoxersWeight + " кг");
         var wightDifference = secondBoxerWeight % firstBoxerWeight;
         System.out.println("Разница веса бойцов " + wightDifference + " кг");
+
+        var totalWorkTime = 640;
+        var eachWorkerTime = 8;
+        var workers = totalWorkTime / eachWorkerTime;
+        System.out.println("Всего работников в компании - " + workers + " человек");
+        var extraWorkers = 94;
+        var totalWorkers = workers + extraWorkers;
+        var newEachWorkerTime = totalWorkTime / totalWorkers;
+        System.out.println("Если в компании работает " + totalWorkers + " человек, то всего " + newEachWorkerTime + " часов работы может быть поделено между сотрудниками");
     }
 }
